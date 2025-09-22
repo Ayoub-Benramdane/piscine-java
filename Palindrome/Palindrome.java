48a7ebf7936832192e7da71e0c9b6c52ae5c83f3
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-22 19:33:10 +0100

    new

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,6 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        System.out.println(s);
         if (s == null) return false;
         String reversed = new StringBuilder(s).reverse().toString();
         return reversed.toUpperCase() == s.toUpperCase(null);
