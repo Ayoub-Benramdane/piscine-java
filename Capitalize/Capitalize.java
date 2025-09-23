ffb5daf030394a6a2dc5e310b99594f42551df9e
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-23 19:22:08 +0100

    new

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        InputStream inputStream = new FileInputStream(args[0]);
+        FileOutputStream outputStream = new FileOutputStream(args[1]);
+        var file = new String(inputStream.readAllBytes());
+        var lines = file.split("\n");
+        List<String> res = new ArrayList<>();
+        for (String line : lines) {
+            var words = line.split(" ");
+            for (String word : words) {
+                res.add(word.substring(0, 1).toUpperCase() + word.substring(1));
+            }
+        }
+    }
+}
