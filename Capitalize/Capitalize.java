9966521735cdd71c5eb6e5518e7cdc3add870755
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-24 11:35:13 +0100

    new

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -4,21 +4,20 @@ import java.util.List;
 
 public class Capitalize {
     public static void capitalize(String[] args) throws IOException {
-        if (args.length != 0) {
-            InputStream inputStream = new FileInputStream(args[0]);
-            FileOutputStream outputStream = new FileOutputStream(args[1]);
-            var file = new String(inputStream.readAllBytes());
-            var lines = file.split("\n");
-            List<String> res = new ArrayList<>();
-            for (String line : lines) {
-                var words = line.split(" ");
-                for (String word : words) {
+        InputStream inputStream = new FileInputStream(args[0]);
+        FileOutputStream outputStream = new FileOutputStream(args[1]);
+        var file = new String(inputStream.readAllBytes());
+        var lines = file.split("\n");
+        List<String> res = new ArrayList<>();
+        for (String line : lines) {
+            var words = line.split(" ");
+            for (String word : words) {
+                if (word.length() > 0)
                     res.add(word.substring(0, 1).toUpperCase() + word.substring(1));
-                }
             }
-            outputStream.write(res.toString().getBytes());
-            inputStream.close();
-            outputStream.close();
         }
+        outputStream.write(res.toString().getBytes());
+        inputStream.close();
+        outputStream.close();
     }
 }
