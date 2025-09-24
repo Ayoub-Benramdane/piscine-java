21842df8adf96e898b7007b5ceee88b3f91635f3
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-24 21:20:12 +0100

    new

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,20 @@
+public class CelestialObject {
+    public String name;
+    public double x;
+    public double y;
+    public double z;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+        this.x = 0.0;
+        this.y = 0.;
+        this.z = .0;
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
