c6b53089618ac1c6dbaab4a04b606b5118e3badb
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-24 18:42:03 +0100

    new

---
+++ FileManager/FileManager.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+        new FileWriter(fileName).write(content);
+        new FileWriter(fileName).close();
+    }
+
+    public static String getContentFile(String fileName) throws IOException {
+        InputStream inputStream = new FileInputStream(fileName);
+        byte[] buffer = new byte[4096];
+        StringBuilder res = new StringBuilder();
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            res.append(new String(buffer, 0, bytesRead));
+        }
+        return res.toString();
+    }
+
+    public static void deleteFile(String fileName) {
+        new File(fileName).delete();
+    }
+}
