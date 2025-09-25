d83941722753472f4450f697d0c44bf9e81e79a6
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-25 14:22:05 +0100

    new

--- FileSearch/FileSearch.java
+++ FileSearch/FileSearch.java
@@ -1,23 +1,16 @@
 import java.io.File;
 
 public class FileSearch {
-    public static String searchFile(String fileName) {
-        String currentDir = "documents";
-        return searchInside(fileName, currentDir);
-    }
-
-    public static String searchInside(String fileName, String directory) {
-        File dir = new File(directory);
+    public static String searchFile(String fileName) {        
+        File dir = new File("doucuments");
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    String result = searchInside(fileName, file.getPath());
+                    String result = searchFile(fileName);
                     if (result != null) {
                         return result;
                     }
-                } else if (file.getName().equals(fileName)) {
-                    return file.getPath();
                 }
             }
         }
