95e1ff2e14d93d453e54060931eec1fbbb6231b6
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-09-25 18:14:15 +0100

    new

---
+++ StarInheritance/Star.java
@@ -0,0 +1,11 @@
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public double setMagnitude(double magnitude) {
+        return this.magnitude;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+}
