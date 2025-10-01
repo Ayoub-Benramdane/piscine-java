f556d2d54ae3de7f07164eb8e43eec6ba465236f
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-10-01 21:39:34 +0100

    new

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -5,8 +5,10 @@ public class ListEquals {
         System.out.println(list1);
         System.out.println(list2);
         if (list1 == null || list2 == null) {
+            System.out.println("w7da null");
             return false;
         } else if (list1 == null && list2 == null) {
+            System.out.println("bjouj null");
             return true;
         }
         return list1.equals(list2);
