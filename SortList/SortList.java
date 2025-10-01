9ff55116d16610226c5d313d1338a03e2d4c81ae
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-10-01 22:03:06 +0100

    new

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -5,13 +5,15 @@ import java.util.List;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        List<Integer> sorted = new ArrayList<>(list); // Make it modifiable
+        if (list == null) return null;
+        List<Integer> sorted = new ArrayList<>(list);
         Collections.sort(sorted);
         return sorted;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        List<Integer> sorted = new ArrayList<>(list); // Make it modifiable
+        if (list == null) return null;
+        List<Integer> sorted = new ArrayList<>(list);
         sorted.sort(Collections.reverseOrder());
         return sorted;
     }
