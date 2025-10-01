05ffc266d2f9a43751f8c60c823a5559e8577220
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-10-01 21:53:15 +0100

    new

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -3,15 +3,11 @@ import java.util.List;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
-        System.out.println(list);
-        System.out.println(value);
-        if (!list.contains(value)) return null;
+        if (list == null || !list.contains(value)) return null;
         return list.lastIndexOf(value);
     }
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
-        System.out.println(list);
-        System.out.println(value);
-        if (!list.contains(value)) return null;
+        if (list == null || !list.contains(value)) return null;
         return list.indexOf(value);
     }
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
