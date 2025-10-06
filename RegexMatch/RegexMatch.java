4cd4eb325d90feb3a2e1fb0196a0c790fdf3dccd
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-10-06 18:17:32 +0100

    new

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,22 @@
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.matches("^[a-zA-Z]+$");
+    }
+    
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.matches("^[a-zA-Z].*\\d$");
+    }
+    
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.matches(".*A{3,}.*");
+    }
+}
